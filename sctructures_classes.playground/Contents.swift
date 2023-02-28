9ee3c8a20293b79bfd8abaf10ac61dc9c93bb216
harald bregu <harald.bregu@gmail.com>
2023-02-28 12:53:28 +0100

    Remove unused files

--- sctructures_classes.playground/Contents.swift
+++ sctructures_classes.playground/Contents.swift
@@ -1,9 +1,70 @@
 import UIKit
 
-func findIndex<T: Equatable>(of valueToFind: T, in array: [T]) -> Int? {
-    return array.firstIndex(of: valueToFind)
+
+class Account {
+        
+    let name: String
+
+    init(name : String) {
+        self.name = name
+    }
+
+    deinit {
+        print("\(name) account has being deallocated")
+    }
 }
 
-let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+class Manager {
+    
+    let name: String
+    var account: Account?
+    
+    init(name : String, account: Account?) {
+        self.name = name
+        self.account = account
+    }
+    
+    deinit {
+        print("\(name) has being deallocated")
+    }
+}
 
-let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
+var manager: Manager? = Manager(name: "John", account: nil)
+var accountManager: Account? = Account(name: "John")
+manager?.account = accountManager
+
+accountManager = nil
+
+if let name = manager?.account?.name {
+    print(name)
+}
+
+
+// Weak reference type
+
+class Employee {
+    var name: String
+    weak var account: Account?
+    
+    init(name : String, account: Account?) {
+        self.name = name
+        self.account = account
+    }
+
+    deinit {
+        print("\(name) has being deallocated")
+    }
+}
+
+var employee: Employee? = Employee(name: "Patrick", account: nil)
+var employeeAccount: Account? = Account(name: "Patrick")
+employee?.account = employeeAccount
+
+employeeAccount = nil
+
+// Whe setting to nil the employee object,
+// the class will be deallocated because it
+// has a weak reference to account object
+if let name = employee?.account?.name {
+    print(name)
+}
